1c2df7866b093e3abe0ce2340376bd1706eb921c
kuicodefly <49471670+kuicodefly@users.noreply.github.com>
2020-06-24 16:27:10 +0800

    登录接口

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,10 +1,16 @@
 import request from '@/utils/request';
+import api from '@/utils/api';
 
 export interface LoginParamsType {
   userName: string;
   password: string;
   mobile: string;
   captcha: string;
+}
+
+export interface LoginData {
+  data: any;
+  method: string;
 }
 
 export async function fakeAccountLogin(params: LoginParamsType) {
@@ -16,4 +22,8 @@ export async function fakeAccountLogin(params: LoginParamsType) {
 
 export async function getFakeCaptcha(mobile: string) {
   return request(`/api/login/captcha?mobile=${mobile}`);
+}
+
+export async function loginAction(params: LoginData):  Promise<any> {
+  return api('public/index.php/loginAction', params);
 }
