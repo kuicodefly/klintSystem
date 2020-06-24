52923efd41443c49aebc2a5f35389da3f959ba55
kuicodefly <49471670+kuicodefly@users.noreply.github.com>
2020-06-24 16:22:54 +0800

    新增菜单

--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -1,9 +1,10 @@
 export default {
-  'menu.welcome': '欢迎',
+  'menu.welcome': '系统声名',
   'menu.more-blocks': '更多区块',
   'menu.home': '首页',
-  'menu.admin': '管理页',
-  'menu.admin.sub-page': '二级管理页',
+  'menu.admin': '权限管理',
+  'menu.admin.sub-page': '账号管理',
+  'menu.productList': '产品列表',
   'menu.login': '登录',
   'menu.register': '注册',
   'menu.register.result': '注册结果',
